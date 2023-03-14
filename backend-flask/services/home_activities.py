0dd19a5b6d9025841b99758977affd164ba42781
Mavewrick <mavewreek@gmail.com>
2023-03-14 16:06:36 +0000

    update sg rule

--- backend-flask/services/home_activities.py
+++ backend-flask/services/home_activities.py
@@ -1,6 +1,6 @@
 from datetime import datetime, timedelta, timezone
 from opentelemetry import trace
-from lib.db import pool
+from lib.db import pool, query_wrap_array
 
 tracer = trace.get_tracer("home.activities")
 
